ec9dcc3b9f7251f54e36de46d0a832a3e7f5a43d
dineysd <dineysd@hotmail.com>
2018-12-27 19:58:30 -0200

    criendo entidade carro para teste

---
+++ src/main/java/br/teste/locacao/entidades/Carro.java
@@ -0,0 +1,10 @@
+package br.teste.locacao.entidades;
+
+public class Carro extends Produto {
+
+	public Carro(String nome, Integer estoque, Double precoLocacao) {
+		super(nome, estoque, precoLocacao);
+	}
+
+	
+}
