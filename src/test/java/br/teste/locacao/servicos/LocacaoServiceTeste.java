5e1e034d55edd6edc00e3babf7178e0c9782567b
dineysd <dineysd@hotmail.com>
2018-12-28 16:02:18 -0200

    desafio, alugar mais de um produto por vez

--- src/test/java/br/teste/locacao/servicos/LocacaoServiceTeste.java
+++ src/test/java/br/teste/locacao/servicos/LocacaoServiceTeste.java
@@ -7,13 +7,13 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertThat;
 import static org.junit.Assert.fail;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Date;
+import java.util.List;
 
-import org.junit.After;
-import org.junit.AfterClass;
 import org.junit.Assert;
 import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ErrorCollector;
@@ -31,6 +31,7 @@ import br.teste.locacao.utils.DataUtils;
 public class LocacaoServiceTeste {
 
 	LocacaoService service;
+	List<Produto> produtos;
 	@Rule
 	public ErrorCollector error = new ErrorCollector();
 	@Rule
@@ -39,33 +40,36 @@ public class LocacaoServiceTeste {
 	@Before
 	public void setup() {//antes
 		service = new LocacaoService();
+		produtos = new ArrayList<Produto>();
 	}
 
 	@Test
 	public void testeLocacao() throws Exception {
 
 		// Cenario onde inicilizaremos as variaveis ou objetos
+		
 		Produto filme = new Filme("A Mumia", 1, 5.00);
+		Produto filme2 = new Filme("Sem Limites", 2, 5.00);
 		Usuario usuario = new Usuario("Carlos");
+		produtos = Arrays.asList(filme, filme2);
 
 		// Ação onde invocaremos o metodo que queremos testar
 
-		Locacao locacao = service.alugar(usuario, filme);
+		Locacao locacao = service.alugar(usuario, produtos);
 	
 		// validação onde vamos verificar se o metodo da ação esta de acordo com o
 		// cenario especificado
 		// ou resultado de acordo com esperado
 
 		error.checkThat(locacao.getUsuario().getNome(), is("Carlos"));// verifique que o nome do usuario é Carlos
-		assertThat(locacao.getValor(), is(equalTo(5.0)));// verifique se o valor é igual a 5.00
+		assertThat(locacao.getValor(), is(equalTo(10.0)));// verifique se o valor é igual a 5.00
 		error.checkThat(locacao.getValor(), is(not(8.0)));// verifique se o valor não é a 8.00
 
 		Assert.assertEquals("usuario corresponde ao mesmo nome: ", "Carlos", locacao.getUsuario().getNome());
-		assertEquals("Produto corresponde ao mesmo nome: ", "A Mumia", locacao.getProduto().getNome());
 		error.checkThat(DataUtils.isMesmaData(locacao.getDataLocacao(), new Date()), is(true));
 		error.checkThat(DataUtils.isMesmaData(locacao.getDataRetorno(), DataUtils.obterDataComDiferencaDias(1)),
 				is(true));
-		Assert.assertEquals("valor do Produto corresponde ao mesmo valor: ", 5.00, locacao.getValor(), 0.01);
+		Assert.assertEquals("valor do Produto corresponde ao mesmo valor: ", 10.00, locacao.getValor(), 0.01);
 
 	}
 
@@ -75,22 +79,28 @@ public class LocacaoServiceTeste {
 
 		// Cenario onde inicilizaremos as variaveis ou objetos
 		Locacao locacao = new Locacao();
-		Produto carro = new Carro("cruze", 0, 75.00);
+		Produto carro = new Carro("Cruze", 0, 75.00);
+		Produto carro2 = new Carro("Civic", 0, 85.00);
 		Usuario usuario = new Usuario("Carlos");
+		produtos = Arrays.asList(carro, carro2);
 
 		// Ação onde invocaremos o metodo que queremos testar
 
-		locacao = service.alugar(usuario, carro);
+		locacao = service.alugar(usuario, produtos);
 	}
 
 	//forma robusta
 	@Test
 	public void testeLocacao_usuarioVazio() throws ProdutoSemEstoqueException {
 		// Cenario
+		
 		Produto filme = new Filme("A Mumia II", 1, 5.00);
+		Produto filme2 = new Filme("Sem Limites", 2, 5.00);
+		Produto filme3 = new Filme("tropa de elite", 3, 5.00);
+		produtos = Arrays.asList(filme, filme2, filme3);
 		// Ação 
 		try {
-			Locacao locacao = service.alugar(null, filme);
+			Locacao locacao = service.alugar(null, produtos);
 			fail("Deveria lançar uma exception!");
 		} catch (LocadoraException e) {
 			assertThat(e.getMessage(),is("Usuario vazio!"));
